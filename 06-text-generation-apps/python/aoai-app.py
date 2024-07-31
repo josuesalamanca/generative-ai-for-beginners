b2b5e689ffcc50830133dba5dad7121f8e65cb7a
josuesalamanca <37229542+josuesalamanca@users.noreply.github.com>
2024-07-30 19:56:37 -0500

    Update aoai-app.py

--- 06-text-generation-apps/python/aoai-app.py
+++ 06-text-generation-apps/python/aoai-app.py
@@ -2,25 +2,25 @@ from openai import AzureOpenAI
 import os
 import dotenv
 
-# import dotenv
+# IMportar dotRNV
 dotenv.load_dotenv()
 
-# configure Azure OpenAI service client 
+# Configurar OpenAI
 client = AzureOpenAI(
   azure_endpoint = os.environ["AZURE_OPENAI_ENDPOINT"], 
   api_key=os.environ['AZURE_OPENAI_KEY'],  
   api_version = "2023-10-01-preview"
   )
 
-deployment=os.environ['AZURE_OPENAI_DEPLOYMENT']
+deployment=os.environ['AZURE_OPENAI_DEPLOYMENT'] # seleccionar tipo de modulo chatgpt v3.5 turbo
 
-# add your completion code
+# COdigo para completar
 prompt = "Complete the following: Once upon a time there was a"
-messages = [{"role": "user", "content": prompt}]  
+messages = [{"role": "user", "content": prompt}] # es como el flow de mensajes, se puede anidar  
 # make completion
-completion = client.chat.completions.create(model=deployment, messages=messages)
+completion = client.chat.completions.create(model=deployment, messages=messages) # obtener la respuesta de la IA
 
-# print response
+# Imprimir respuesta
 print(completion.choices[0].message.content)
 
 #  very unhappy _____.
